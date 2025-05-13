966313e9d09b839d1d40fef6770d88540214f8ad
Spring <s1170082@usls.edu.ph>
2025-05-14 04:59:04 +0800

    FIXED: Logout; ADDED: [AdminSide] AddStudent(CRUD), Sidebar; ISSUES: Can't upload Pictures

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -3,10 +3,12 @@
     <ul>
         <li><a href="{{ route('dashboard') }}"><i class="fas fa-home"></i> Home</a></li>
         <li><a href="{{ route('students.index') }}"><i class="fas fa-user"></i> Students</a></li>
-
     </ul>
     <div id="user-info">
         <p>Welcome, {{ session('user_role') }}</p>
-        <p><a href="{{ route('logout') }}" class="logout-btn">Logout</a></p>
+        <form action="{{ route('logout') }}" method="POST" style="display: inline;">
+            @csrf
+            <button type="submit" class="logout-btn">Logout</button>
+        </form>
     </div>
 </div>
